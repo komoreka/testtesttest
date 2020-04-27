63ac7932b807e65473aa8f105ad97616545bb3e8
komoreka <51116931+komoreka@users.noreply.github.com>
2020-04-27 20:56:14 +0200

    fixed

--- HybridWorkerConnection.ps1
+++ HybridWorkerConnection.ps1
@@ -15,8 +15,5 @@ install-module az -confirm:$false
 cd "C:\Program Files\Microsoft Monitoring Agent\Agent\AzureAutomation\*\HybridRegistration"
 Import-Module .\HybridRegistration.psd1
 Add-HybridRunbookWorker –GroupName $GroupName -EndPoint $EndPoint -Token $Token
-<<<<<<< HEAD
+
 }
-=======
-}
->>>>>>> 810cdd1e8e4246e4dadd8c4b1d98507c030095e0
